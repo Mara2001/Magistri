0cc90dfa5ccb0aacb7c6bd03196a12cd046085b2
Mara2001 <nedvem@gmail.com>
2025-01-04 18:42:48 +0100

    fix: bug with login the user without role

--- Magistri/Controllers/HomeController.cs
+++ Magistri/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Magistri.Controllers
 {
-    [Authorize(Roles = "Student, Teacher, Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
